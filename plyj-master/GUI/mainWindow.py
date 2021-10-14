4aaa32c666eb566ec9b7efa0135ed875399dacb5
jlew92 <77688482+jlew92@users.noreply.github.com>
2021-10-14 15:47:09 -0500

    Added the help function
    Help includes instructions

--- plyj-master/GUI/mainWindow.py
+++ plyj-master/GUI/mainWindow.py
@@ -4,7 +4,7 @@ from PyQt5 import QtWidgets as qtw
 from UIFiles.GCMainWindowGUI import Ui_MainWindow
 from PyQt5 import QtCore as qtc
 import os
-from PyQt5.QtWidgets import QFileDialog, QAbstractItemView
+from PyQt5.QtWidgets import QFileDialog, QAbstractItemView, QDialog, QLabel
 
 
 
@@ -15,14 +15,17 @@ class MainWindow(qtw.QMainWindow):
         super(MainWindow,self).__init__(*args, **kwargs)
         ui = Ui_MainWindow()
         ui.setupUi(self)
+        ui.actionInstruction.associatedGraphicsWidgets
 
         ## Adds the function to the button.
         ui.SubmitFileLink.clicked.connect(self.uploadSingleFile)
+        ui.actionInstruction.triggered.connect(self.openHelp)
         ui.javaFileTable.setSelectionBehavior(QAbstractItemView.SelectRows)
 
 
     #Uploads a single java file to the parser.
     def uploadSingleFile(self):
+
         file_filter = 'Java File(*.java)'
         filePath = QFileDialog.getOpenFileName(
             parent = self, caption = 'Select a Java File', 
@@ -32,7 +35,15 @@ class MainWindow(qtw.QMainWindow):
 
         print(filePath[0])
     
-   
+    #Opens the dialog for help
+    def openHelp(self):
+        dlg = QDialog()
+        dlg.setWindowTitle("Help")
+        label = QLabel(dlg)
+        label.setText("Help\n instructions: \nThis program does shit. Figure it out for you self")
+        label.adjustSize()
+        label.move(100, 60)
+        dlg.exec_()
 
 
 if __name__ == '__main__':
