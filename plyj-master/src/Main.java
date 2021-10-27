604f9ab66039a7ff8ee3b23adb82f99624da4a00
jlew92 <77688482+jlew92@users.noreply.github.com>
2021-10-27 14:31:50 -0500

    Random Java

---
+++ plyj-master/src/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+
+
+
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int x = 2;
+        while (x < 100) {
+            if (5 > x) {
+                x++;
+
+            } else {
+
+
+                if (x == 42) {
+                    if (true) {
+                        int y = 9;
+                    }
+                    x += 12;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
