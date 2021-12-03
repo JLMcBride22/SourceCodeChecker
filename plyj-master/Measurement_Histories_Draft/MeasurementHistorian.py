5215abc6c7ac6a52c96f9ff533f6d7e8c8c44d87
Crunch1411 <62269145+Crunch1411@users.noreply.github.com>
2021-12-03 13:13:22 -0600

    Add files via upload

--- plyj-master/Measurement_Histories_Draft/MeasurementHistorian.py
+++ plyj-master/Measurement_Histories_Draft/MeasurementHistorian.py
@@ -1,9 +1,17 @@
+# MeasurementHistorian.py
+# Purpose: To handle the primary management of the SQL database for our program reports.
+#          This includes creating tables, storing data, retrieving data, and certain data management checks.
+# Author: James T. Kinkead
+# Build Date: December 3, 2021.
+#********************************
+
+
+
+
 import os
 import sqlite3
 import datetime
 from sqlite3 import Error
-
-# IF this  has module issues let know immediately.
 
 
 #This class will handle all the methods for storing/searching reports.
@@ -92,66 +100,9 @@ class MeasurementHistorian:
             self.create_table(conn, sql_create_projects_table )
         else:
             print("Error! cannot create the database connection.")
-    #************************************************************
+    
 
-    def create_function_table(self, conn):
-        sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS FunctionReports (
-                                        id integer PRIMARY KEY,
-                                        filename text NOT NULL,
-                                        timestamp text,
-                                        datasize text,
-                                        DateAnalyzed text,
-                                        ESLOC integer,
-                                        SLOCnoComm integer,
-                                        SLOCComm integer,
-                                        BlankLines integer,
-                                        FullCommLines integer,
-                                        Semicolons integer,
-                                        FunctionCalls integer,
-                                        NumPassedParam integer,
-                                        McCabeCyclComp integer,
-                                        Halstead integer,
-                                        MaxNest integer,
-                                        ESLOCMaxNest integer,
-                                        SwitchComp text,
-                                        NumForLoop integer,
-                                        NumWhileLoop integer,
-                                        NumRepeatLoop integer,
-                                        NumInts integer,
-                                        NumFloat integer,
-                                        NumChar integer,
-                                        NumString integer,
-                                        NumUserDef integer,
-                                        NumStruct integer,
-                                        NumArray integer,
-                                        Num3Char integer,
-                                        Num3thru9Char integer,
-                                        Num10thru19Char integer,
-                                        Num20Char integer,
-                                        PreambleFilename text,
-                                        PreambleAuthor text,
-                                        PreamblePurpose text,
-                                        PreambleInterface text,
-                                        PreambleAssumptions text,
-                                        PreambleChangeLog text,
-                                        NoGoTo text,
-                                        OneEntry text,
-                                        OneExit text,
-                                        RecursionStatus text,
-                                        VariableNamesAtLeastXChar text,
-                                        VariableNamesNoLongXChar text,
-                                        DefineParamAllCAPS text,
-                                        VarNamesNotAllCAPS text,
-                                        McCabeLessThanX text,
-                                        NestingLessThanX text,
-                                        ESLOCLessThanXinFunc text,
-                                        LocalizationOfVar text
-
-                                    ); """
-        if conn is not None:
-            self.create_table(conn, sql_create_projects_table )
-        else:
-            print("Error! cannot create the database connection.")
+    
     #*************************************************************************
     # Take an analysis report and add it to our report table. 
     def create_analysis_report(conn, report):
@@ -206,59 +157,8 @@ class MeasurementHistorian:
         conn.commit()
         return cur.lastrowid
 
-
-    def create_function_report(conn, report):
-        sql = ''' INSERT INTO FunctionReports(filename, timestamp, datasize, DateAnalyzed, ESLOC,
-                                        SLOCnoComm,
-                                        SLOCComm,
-                                        BlankLines,
-                                        FullCommLines,
-                                        Semicolons,
-                                        FunctionCalls,
-                                        NumPassedParam,
-                                        McCabeCyclComp,
-                                        Halstead,
-                                        MaxNest,
-                                        ESLOCMaxNest,
-                                        SwitchComp,
-                                        NumForLoop,
-                                        NumWhileLoop,
-                                        NumRepeatLoop,
-                                        NumInts,
-                                        NumFloat,
-                                        NumChar,
-                                        NumString,
-                                        NumUserDef,
-                                        NumStruct,
-                                        NumArray,
-                                        Num3Char,
-                                        Num3thru9Char,
-                                        Num10thru19Char,
-                                        Num20Char,
-                                        PreambleFilename,
-                                        PreambleAuthor,
-                                        PreamblePurpose,
-                                        PreambleInterface,
-                                        PreambleAssumptions,
-                                        PreambleChangeLog,
-                                        NoGoTo,
-                                        OneEntry,
-                                        OneExit,
-                                        RecursionStatus,
-                                        VariableNamesAtLeastXChar,
-                                        VariableNamesNoLongXChar,
-                                        DefineParamAllCAPS,
-                                        VarNamesNotAllCAPS,
-                                        McCabeLessThanX,
-                                        NestingLessThanX,
-                                        ESLOCLessThanXinFunc,
-                                        LocalizationOfVar)
-              VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) '''
-        cur = conn.cursor()
-        cur.execute(sql, report)
-        conn.commit()
-        return cur.lastrowid          
-
+#****************************************************************************************************
+   
 
     # Debugging function. Will print the entire contents of the database to the console.
     def print_all_reports(conn):
@@ -368,23 +268,29 @@ class MeasurementHistorian:
         else:
             return False
 
+
+    # This function pulls most of the data from rows going into the history window.
+    # Takes an IDList, which should be sorted by pullHistory.
+    # In future updates, would like this and pullHistory to be a single function.
     def pullHistoryContent(self, conn, IDList):
         cur = conn.cursor()
         contentList = []
         i = 0
+
+        # loop through each iteration of the specified file and grab the needed information.
         for ID in IDList:
             rows = cur.execute("SELECT * FROM AnalysisReports WHERE ID = ?", (IDList[i],))
             for row in rows:
                 j = 0
                 rowList = []
-                while j < 49:
 
-                    # This is a messy, quick step to skip grabbing the filename.
-                    # Need to change later.
+                # Skip the last entry because the xml string is not needed.
+                while j < (len(row) - 1):
+
+                    # We do not want the filename.
                     if j != 1:
                         rowList.append(row[j])
-                    # This is a messy, quick step to skip grabbing the filename.
-                    #
+                   
                     j+=1
                 contentList.append(rowList)
                     
@@ -394,29 +300,4 @@ class MeasurementHistorian:
 
             
 
-if __name__ == '__main__':
-    mhist = MeasurementHistorian
-    dataconn = mhist.create_connection("testHistory.db")
-    with dataconn:
 
-        report1 = ("Test", "Hello", "filler", datetime.datetime.now(), 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46)
-        report2 = ("Test", "Hello", "filler", datetime.datetime.now(), 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46)
-        report3 = ("Hello", "Hello", "filler", datetime.datetime.now(), 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46)
-        report4 = ("Test", "Hello", "filler", datetime.datetime.now(), 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46)
-        mhist.create_analysis_table(mhist, dataconn)
-        mhist.create_function_table(mhist, dataconn)
-        #mhist.create_analysis_report(dataconn, report1)
-        #mhist.create_analysis_report(dataconn, report2)
-        #mhist.create_analysis_report(dataconn, report3)
-        #mhist.create_analysis_report(dataconn, report4)
-        #tempcheck = mhist.entry_exists(dataconn, "pizza")
-        mhist.print_all_reports(dataconn)
-        #tempList = mhist.search_archive_filename(mhist, dataconn, "Java")
-        #for Id in tempList:
-            #   print(Id)
-        idList = mhist.search_archive_filename_substring(mhist, dataconn, "He")
-        print(idList)
-
-
-
-        # NEED TO UPDATE FOR BYTE SIZE AND UPDATE THE EXCEL CONVERTER.
