c2fa54b90a5b5868bd859c0c9e6f719197cc6d35
Crunch1411 <62269145+Crunch1411@users.noreply.github.com>
2021-12-03 13:57:45 -0600

    Add files via upload

--- plyj-master/GUI/history.py
+++ plyj-master/GUI/history.py
@@ -1,4 +1,10 @@
-
+# history.py
+# Purpose: To display the history of a file's analysis
+#          That means displaying every single report corresponding to a particular filepath.
+#          Does so with the help of information obtained by MeasurementHistorian.py.
+# Authors: James T. Kinkead, Jonathan Lewis.
+# Build Date: December 3, 2021.
+#********************************************************************************************
 import PyQt5.QtGui as testing
 from PyQt5 import QtWidgets as qtw
 
@@ -12,6 +18,7 @@ from UIFiles.GCFileSubGUI import Ui_FileSubForm
 
 from Measurement_Histories_Draft.MeasurementHistorian import MeasurementHistorian
 
+# Class that handles the formatting of the history window.
 class historyForm(qtw.QWidget):
     
     uiForm = Ui_HistoryForm()
@@ -21,6 +28,8 @@ class historyForm(qtw.QWidget):
         
         self.uiForm.setupUi(self)
         j = 0
+        # This loop controls how many columns you wish to display. Currently corresponds with the list below.
+        # Currently does not directly utilize the list of column headers, due to earlier bugs.
         while j < 45:
             self.uiForm.tableWidget.insertColumn(j)
             j += 1
@@ -69,28 +78,24 @@ class historyForm(qtw.QWidget):
                                         "NestingLessThanX",
                                         "ESLOCLessThanXinFunc",
                                         "LocalizationOfVar"])
-        #mhist = MeasurementHistorian()
-        #tempconn = mhist.create_connection(mhist, "test3.db")
-        historyList = []
-        #historyList =  mhist.pullHistory(mhist, tempconn, "Test")
         
-
-        
-        #historyList =  mhist.pullHistory(mhist, tempconn, "Test")
+#*******************************************************************      
         
         
             
-
+    # This loads the rows of the table, corresponding with the headers in the previous version.
     def loadHistTable(self, contentList):
         i = 0
+        # For each report, load the information into the row.
         for row in contentList:
             
+            # Have to first insert the row for the data
             self.uiForm.tableWidget.insertRow(i)
             
             col = 0
             k = 1
             while col < (len(row)-1):
-                
+                # For each item in the report, insert it.
                 self.uiForm.tableWidget.setItem(i, col, qtw.QTableWidgetItem(str(row[k])))
                 
                 col += 1
